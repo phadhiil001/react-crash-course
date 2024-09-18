3ad340e6af0250ec87d5e51017fadbbb33ca13eb
phadhiil001 <phadhiil001@gmail.com>
2024-09-18 10:49:55 -0500

    Added the modal for the form

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+import NewPost from './NewPost';
 import Post from './Post';
+import Modal from './Modal'
 import classes from './PostList.module.css';
 
 function PostList () {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function hideModalHnadler() {
+        setModalIsVisible(false);
+    }
+
     return (
-        <ul className={classes.posts}>
-            <Post author="Fadlullah" body="React Js" />
-            <Post author="Jamiu" body="Welcome back"/>
-            <Post author="Imam" body="Nice one" />
-            <Post author="Umar" body="Bin Khattab"/>
-        </ul> 
+        <>
+        {modalIsVisible && 
+            (<Modal onClose={hideModalHnadler}>
+                <NewPost 
+                    onBodyChange={changeBodyHandler}  
+                    onAuthorChange={changeAuthorHandler} 
+                />
+            </Modal>
+        )}
+            
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Jamiu" body="Welcome back"/>
+                <Post author="Imam" body="Nice one" />
+                <Post author="Umar" body="Bin Khattab"/>
+            </ul> 
+            
+        </>
     );
 }
 
